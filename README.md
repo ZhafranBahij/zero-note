21c252af927c273544881ac2b374b4e157afa5e4
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2021-10-07 11:39:10 +0700

    add form , navbar, and table

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Zero-Note
 
 Well, i create this app with react and tailwind.
+
+## Some Technical that i need in the future
+
+create global.d.ts in the rood of directory and add in this declare module "@tailwindcss/forms";
+I remove a purge in package.json
+The rest i just took it from tutorial
---
+++ src/dataJSON/hero.js
@@ -0,0 +1,19 @@
+const Hero = [
+  {
+    name: "Jane Cooper",
+    skill: "Dragon Samadhi",
+    role: "Admin",
+  },
+  {
+    name: "Luciana Pecora",
+    skill: "Mighty Slasher",
+    role: "Admin",
+  },
+  {
+    name: "Produguise",
+    skill: "Zero no Senko",
+    role: "Defender",
+  },
+];
+
+export default Hero;
